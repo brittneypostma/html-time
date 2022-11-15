0f8e35709b5035ff14910143cd9e0ca74e38126f
brittneypostma <bdesignedwebdev@gmail.com>
2022-11-15 09:24:27 -0500

    try catch block

--- netlify/edge-functions/time.ts
+++ netlify/edge-functions/time.ts
@@ -25,14 +25,18 @@ export default async (context: Context) => {
 		hour: 'numeric',
 		minute: 'numeric'
 	})
-	// Get the page content
-	const response = await context.next()
-	const page = await response.text()
+	try {
+		// Get the page content
+		const response = await context.next()
+		const page = await response.text()
 
-	// Replace the content
-	const regex_time = /CURRENT_TIME/gi
-	const regex_place = /CURRENT_LOCATION/gi
-	let updatedPage = page.replace(regex_time, time)
-	updatedPage = updatedPage.replace(regex_place, locationLabel)
-	return new Response(updatedPage, response)
+		// Replace the content
+		const regex_time = /CURRENT_TIME/gi
+		const regex_place = /CURRENT_LOCATION/gi
+		let updatedPage = page.replace(regex_time, time)
+		updatedPage = updatedPage.replace(regex_place, locationLabel)
+		return new Response(updatedPage, response)
+	} catch (e) {
+		return new Error(e.message)
+	}
 }
